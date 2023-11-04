c77bfdcc035e98404a7f8fd909b17902801a3f09
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-11-04 11:48:22 +0800

    Update ss1.sh

--- ss1.sh
+++ ss1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Shadowsocks installation script for various Linux distributions
+# Shadowsocks-libev auto-installation script
 
-# Define Shadowsocks config variables
+# Shadowsocks config variables
 SS_PASSWORD="19898"
 SS_PORT=19898
 SS_METHOD="aes-256-gcm"
@@ -11,11 +11,10 @@ SS_METHOD="aes-256-gcm"
 function install_shadowsocks() {
     if [[ -f /etc/debian_version ]]; then
         # Debian/Ubuntu
-        apt update
-        apt install -y shadowsocks-libev
+        apt-get update
+        apt-get install -y shadowsocks-libev
     elif [[ -f /etc/redhat-release ]]; then
-        # CentOS/Redhat/Fedora
-        yum update
+        # CentOS/RHEL/Fedora
         yum install -y epel-release
         yum install -y shadowsocks-libev
     else
@@ -26,18 +25,22 @@ function install_shadowsocks() {
 
 # Configure Shadowsocks
 function configure_shadowsocks() {
+    # Create Shadowsocks config directory if it doesn't exist
+    mkdir -p /etc/shadowsocks-libev
+
     # Create Shadowsocks config file
     local ss_config="/etc/shadowsocks-libev/config.json"
     cat > $ss_config <<- EOF
-    {
-        "server":"0.0.0.0",
-        "server_port":${SS_PORT},
-        "password":"${SS_PASSWORD}",
-        "timeout":300,
-        "method":"${SS_METHOD}",
-        "fast_open": false,
-        "nameserver":"8.8.8.8"
-    }
+{
+    "server":"0.0.0.0",
+    "server_port":${SS_PORT},
+    "password":"${SS_PASSWORD}",
+    "timeout":300,
+    "method":"${SS_METHOD}",
+    "fast_open":false,
+    "mode":"tcp_and_udp",
+    "nameserver":"8.8.8.8"
+}
 EOF
     # Set permissions for the config file
     chmod 644 $ss_config
@@ -46,7 +49,22 @@ EOF
 # Enable and start Shadowsocks service
 function start_shadowsocks() {
     systemctl enable shadowsocks-libev
-    systemctl start shadowsocks-libev
+    systemctl restart shadowsocks-libev
+}
+
+# Open firewall ports
+function setup_firewall() {
+    if command -v ufw >/dev/null; then
+        ufw allow ${SS_PORT}/tcp
+        ufw allow ${SS_PORT}/udp
+        ufw reload
+    elif command -v firewall-cmd >/dev/null; then
+        firewall-cmd --permanent --add-port=${SS_PORT}/tcp
+        firewall-cmd --permanent --add-port=${SS_PORT}/udp
+        firewall-cmd --reload
+    else
+        echo "Firewall not found. Please manually open the ports if necessary."
+    fi
 }
 
 # Check if the script is running as root
@@ -56,6 +74,7 @@ if [[ $EUID -ne 0 ]]; then
 else
     install_shadowsocks
     configure_shadowsocks
+    setup_firewall
     start_shadowsocks
     echo "Shadowsocks has been installed and started."
 fi
